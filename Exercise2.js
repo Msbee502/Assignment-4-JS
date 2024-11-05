345d0faa554839f590928c5ad3324b5e8efacf71
Msbee502 <b.hoogesteyn@gmail.com>
2024-11-05 16:05:47 +0100

    Adding third part of assignment

--- Exercise2.js
+++ Exercise2.js
@@ -6,47 +6,83 @@
 
 // Return the array from the function. 
 
-function generateRandomArray(){
-
-    let RandomArray = [5]
-    for 
+function generateRandomArray(length){
+    const numbers = []
+    for (i=0; i < length; i++){
+        numbers.push(Math.floor(Math.random() * 100)+1);
+    }
+    return numbers;
 }
 
-// Array
+console.log(generateRandomArray(5));
+
+// This JavaScript code defines a function called generateRandomArray that creates and returns an array of random integers. Here’s a breakdown of what the code does, particularly with the provided call console.log(generateRandomArray(0));:
+
+// Code Breakdown
+// Function Definition:
+
+// function generateRandomArray(length) defines a function that takes one parameter, length. This parameter specifies how many random numbers to generate in the array.
+// Array Initialization:
+
+// const numbers = [] initializes an empty array named numbers, which will be used to store the generated random integers.
+// For Loop:
+
+// for (i = 0; i < length; i++) sets up a loop that will execute length times:
+// It starts with i set to 0.
+// The loop continues as long as i is less than length.
+// After each iteration, i is incremented by 1.
+// Random Number Generation:
+
+// Inside the loop, numbers.push(Math.floor(Math.random() * 100) + 1) generates a random integer between 1 and 100:
+// Math.random() produces a floating-point number between 0 (inclusive) and 1 (exclusive).
+// Multiplying by 100 scales this number to a range between 0 and 100 (exclusive).
+// Math.floor() rounds this number down to the nearest whole number, resulting in values from 0 to 99.
+// Adding 1 shifts the range to be between 1 and 100 (inclusive).
+// The random number is then added to the numbers array using push.
+// Return Statement:
+
+// return numbers; returns the populated numbers array.
+
+
+
+
+
+
+// // Array
+// // let fruit = ["apple", "orange", "banana"];
+
+// // console.log(fruit[2]);
+
+// // Change arrays
+// // let fruit = ["apple", "orange", "banana"];
+
+// // fruit[0] = "kiwi";
+
+// // console.log(fruit[0]);
+
+// //Add to arrays
+// // let fruit = ["apple", "orange", "banana"];
+
+// // fruit.push("mango", "kiwi");
+// //This adds to the beginning of the array
+// // fruit.unshift("coconut");
+// //Remove
+// // fruit.shift();
+
+// // console.log(fruit);
+
+// //Remove from array
+// // let fruit = ["apple", "orange", "banana"];
+
+// // fruit.pop()
+// // console.log(fruit);
+
 // let fruit = ["apple", "orange", "banana"];
+// fruit.reverse(); //Ändrar ordningen
 
-// console.log(fruit[2]);
-
-// Change arrays
-// let fruit = ["apple", "orange", "banana"];
-
-// fruit[0] = "kiwi";
-
-// console.log(fruit[0]);
-
-//Add to arrays
-// let fruit = ["apple", "orange", "banana"];
-
-// fruit.push("mango", "kiwi");
-//This adds to the beginning of the array
-// fruit.unshift("coconut");
-//Remove
-// fruit.shift();
-
-// console.log(fruit);
-
-//Remove from array
-// let fruit = ["apple", "orange", "banana"];
-
-// fruit.pop()
-// console.log(fruit);
-
-let fruit = ["apple", "orange", "banana"];
-fruit.reverse(); //Ändrar ordningen
-
-console.log(fruit.indexOf("apple")); //Visar vilket nummer appel har
-console.log(fruit.length);//Visar hur många index som finns i arrayen
-console.log(fruit.indexOf("orange")); //Visar vilken plats eller indexnummer en vara har
+// console.log(fruit.indexOf("apple")); //Visar vilket nummer appel har
+// console.log(fruit.length);//Visar hur många index som finns i arrayen
+// console.log(fruit.indexOf("orange")); //Visar vilken plats eller indexnummer en vara har
 
 
 
@@ -82,12 +118,12 @@ console.log(fruit.indexOf("orange")); //Visar vilken plats eller indexnummer en 
 // }
 
 //Denna funktion visar också ojämna nummer
-for (let i = 1; i <= 10; i+=2){    
-    console.log(i)
+// for (let i = 1; i <= 10; i+=2){    
+//     console.log(i)
     
-}
-//Och denna visar jämna nummer
-for (let i = 2; i <= 10; i+=2){    
-    console.log(i)
+// }
+// //Och denna visar jämna nummer
+// for (let i = 2; i <= 10; i+=2){    
+//     console.log(i)
     
-}
+// }
